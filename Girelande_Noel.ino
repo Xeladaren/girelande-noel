1d87370bef7e3bd6065a949b8c82a97c1821d180
Xeladaren <xeladaren@pm.me>
2024-12-11 07:37:44 +0100

    version 1

---
+++ Girelande_Noel.ino
@@ -0,0 +1,135 @@
+
+#include <Adafruit_NeoPixel.h>
+#include "images.h"
+
+#define CONTOUR_PIN        6
+#define CONTOUR_NUMPIXELS 144
+
+#define SCREEN_PIN        5
+#define SCREEN_NUMPIXELS 64
+
+Adafruit_NeoPixel contour(CONTOUR_NUMPIXELS, CONTOUR_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel screen(SCREEN_NUMPIXELS, SCREEN_PIN, NEO_GRB + NEO_KHZ800);
+bool state_screen_table[CONTOUR_NUMPIXELS];
+
+void run_contout(int brightness);
+void run_screen(int brightness);
+
+// the setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(9600);
+
+  contour.begin();
+  contour.clear();
+
+  screen.begin();
+  screen.clear();
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+
+  static int run_count = 0;
+  int br = (analogRead(A0) * 128 ) / 1024;
+
+//  Serial.print("rum count = ");
+//  Serial.println(run_count);
+
+  if(run_count % 128 == 0)
+  {
+    run_screen(br/4);
+  }
+
+  run_contout(br);
+  
+  run_count += 1;
+  delay(5);
+}
+
+void run_screen(int brightness)
+{
+  static int image_index = 0;
+  static int sequence[] = {
+      SNOWMAN, 
+      SNOWMAN,
+      YELLOW_J,
+      YELLOW_O,
+      YELLOW_Y,
+      YELLOW_E,
+      YELLOW_U,
+      YELLOW_X,
+      PRESENT,
+      PRESENT,
+      BLUE_N,
+      BLUE_O,
+      BLUE_E,
+      BLUE_L,
+      LUTIN,
+      LUTIN,
+      RED_B,
+      RED_S,
+      RED_P,
+      PIKA,
+      PIKA,
+      GREEN_A,
+      GREEN_C,
+      GREEN_T,
+      GREEN_I,
+      GREEN_V,
+      GREEN_A,
+      GREEN_T,
+      GREEN_E,
+      GREEN_U,
+      GREEN_R
+    };
+
+  int image_num = sequence[image_index];
+  int r, g, b;
+
+  screen.setBrightness(brightness);
+
+  Serial.print("image : ");
+  Serial.print(IMAGE_TO_STR(image_num));
+  Serial.print(" ");
+  Serial.println(image_num);
+
+  for(int i = 0; i < screen.numPixels(); i++)
+  {
+    r = pgm_read_word(&images[image_num][i][0]) ;
+    g = pgm_read_word(&images[image_num][i][1]) ;
+    b = pgm_read_word(&images[image_num][i][2]) ;
+
+    screen.setPixelColor(i, screen.Color(r, g, b));
+  }
+  screen.show();
+  
+  image_index = ( image_index + 1 ) % (sizeof(sequence) / sizeof(int)) ;
+}
+
+void run_contout(int brightness)
+{
+  static bool state_table[CONTOUR_NUMPIXELS];
+  int r, g, b, i;
+
+  r = random(255);
+  g = random(255);
+  b = random(255);
+  i = random(contour.numPixels());
+
+//  Serial.println("run_contout()");
+
+  contour.setBrightness(brightness);
+
+  if(state_table[i] == true)
+  {
+    contour.setPixelColor(i, contour.Color(0, 0, 0));
+    contour.show();
+    state_table[i] = false;
+  }
+  else
+  {
+    contour.setPixelColor(i, contour.Color(r, g, b));
+    contour.show();
+    state_table[i] = true;
+  }
+}
---
+++ img_convert.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+import numpy as np
+import argparse
+import os.path
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--output', "-o", default="images.h")
+parser.add_argument('--rotate', "-r", type=int, default=0)
+parser.add_argument('image', nargs='+')
+
+args = parser.parse_args()
+
+define_str = ""
+index_to_str = "\n#define IMAGE_TO_STR(index)	\\\n"
+image_str = f"\nconst int images_count = {len(args.image)};\n"
+
+image_str += f"\nconst uint8_t PROGMEM images[{len(args.image)}][64][3] = " + "{"
+is_first_image = True
+index = 0
+
+for image_path in args.image:
+
+    im = np.array(Image.open(image_path).convert('RGB').rotate(args.rotate))
+    file_name = os.path.basename(image_path).split(".")[0]
+
+    define_str   += f"#define {file_name:10} 0x{index:02X}\n"
+    index_to_str += f"\tindex == 0x{index:02X} ? \"{file_name}\" : \\\n"
+
+    if is_first_image:
+        is_first_image = False
+    else:
+        image_str += ","
+    image_str += f"\n\t/* {index:02} {file_name:10} */ " + "{"
+
+    is_first_pixel = True
+
+    for ligne in im:
+        for pixel in ligne:
+            if is_first_pixel:
+                image_str += "{"
+                is_first_pixel = False
+            else:
+                image_str += ", {"
+            image_str += f"0x{pixel[0]:02X}, 0x{pixel[1]:02X}, 0x{pixel[2]:02X}"
+            image_str += "}"
+    image_str += "}"
+
+    index += 1
+
+
+image_str += "\n};\n\n"
+index_to_str += f"\t\"Unknow\"\n"
+
+file_header = open("images.h", "w")
+file_header.write(image_str)
+file_header.write(define_str)
+file_header.write(index_to_str)
+file_header.close()
